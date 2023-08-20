a1cd4112c541b8613e11776846b1f51e4a797fe0
Mauricio <mauricio.alvarado@pucp.edu.pe>
2023-08-19 20:15:39 -0500

    Nowcast with july data

---
+++ 1_get_data.py
@@ -0,0 +1,76 @@
+import pandas as pd
+from econdata import BCRP, FRED, YFinance
+import warnings
+warnings.simplefilter('ignore')
+
+
+fechaini     = '2015-01-01'
+fechafin     = '2021-07-31' # Be exact in the last day of the month
+fred_key     = '###############################' # https://fred.stlouisfed.org/docs/api/api_key.html
+
+
+# Data ###################################################################################################
+codes = pd.read_excel('series.xlsx', sheet_name='Series')
+
+''' Group of variables
+codes[['Group', 'Description']].groupby('Group', sort=False).count()
+'''
+
+codes_bcrp = codes.loc[codes['Source'] == 'BCRP']
+codes_fred = codes.loc[codes['Source'] == 'FRED']
+codes_yf   = codes.loc[codes['Source'] == 'YF'  ]
+
+codes_bcrp_mensual = codes_bcrp.loc[codes['Frequency'] == 'Mensual']
+codes_fred_mensual = codes_fred.loc[codes['Frequency'] == 'Mensual']
+codes_yf_diario    = codes_yf.loc  [codes['Frequency'] == 'Diario' ]
+
+dict_bcrp_mensual = list(codes_bcrp_mensual[['Code', 'Description']].set_index('Code').to_dict().values())[0]
+dict_fred_mensual = list(codes_fred_mensual[['Code', 'Description']].set_index('Code').to_dict().values())[0]
+dict_yf_diario    = list(codes_yf_diario   [['Code', 'Description']].set_index('Code').to_dict().values())[0]
+
+
+
+# Importation #############################################################################################
+rango_fechas = pd.period_range(fechaini, fechafin, freq='M')
+
+# Get data
+df_bcrp_mensual  = BCRP.get_data(dict_bcrp_mensual, fechaini=fechaini, fechafin=fechafin)
+df_fred_mensual  = FRED.get_data(dict_fred_mensual, api_key=fred_key, fechaini=fechaini, fechafin=fechafin)
+df_yf_diario     = YFinance.get_data(dict_yf_diario, fechaini=fechaini, fechafin=fechafin)
+
+# Date format
+df_bcrp_mensual.index = pd.to_datetime(df_bcrp_mensual.index).strftime('%Y-%m')
+df_fred_mensual.index = pd.to_datetime(df_fred_mensual.index).strftime('%Y-%m')
+df_yf_diario.index = pd.to_datetime(df_yf_diario.index)
+df_yf_diario = df_yf_diario.resample('M', axis = 0).mean()
+df_yf_diario.index = df_yf_diario.index.strftime('%Y-%m')
+
+
+
+# Join ####################################################################################################
+df = pd.concat(
+    [
+        df_bcrp_mensual,
+        df_fred_mensual,
+        df_yf_diario
+    ],
+    axis = 1
+)
+
+df.index = rango_fechas
+
+
+# GDP
+gdp = BCRP.get_data(
+    {'PN02538AQ': 'Real GDP'},
+    fechaini='1980Q1',
+    fechafin='2023Q1'
+)
+
+
+# Save at the first days after the closing date of the month
+df.to_pickle('Data/monthly/data_2023_07.pkl')
+gdp.to_pickle('Data/quarterly/gdp_2023_07.pkl')
+
+# df.to_csv('Data/monthly/data_2023_07.csv', encoding='UTF-8')
+# gdp.to_csv('Data/quarterly/gdp_2023_07.csv', encoding='UTF-8')
---
+++ 2_transformation.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import numpy as np
+from statsmodels.tsa.seasonal import STL
+from sklearn.preprocessing import StandardScaler
+
+
+df    = pd.read_pickle('Data/monthly/data_2023_07.pkl')
+gdp   = pd.read_pickle('Data/quarterly/gdp_2023_07.pkl')
+codes = pd.read_excel('series.xlsx')
+df_c  = pd.DataFrame(index=df.index, columns=df.columns)
+
+
+# Transformations #############################################################
+'''
+1: Levels
+2: First Difference to the Levels
+3: Logarithm
+4: First Difference to the Logarithm
++ : Season-Trend decomposition using LOESS (STL)
+'''
+
+
+dict_transf = list(
+    codes[['Description', 'Tcode']].set_index('Description').to_dict().values()
+)[0]
+
+
+for k, v in dict_transf.items():
+    
+    X = df[k].dropna()
+
+    if  str(v) == '1':
+        df_c[k] = X
+
+    elif str(v) == '1+':
+        df_c[k] = STL(X, period=12).fit().trend
+    
+    elif str(v) == '2':
+        df_c[k] = X - X.shift(1)
+    
+    elif str(v) == '2+':
+        X = STL(X, period=12).fit().trend
+        df_c[k] = X - X.shift(1)
+    
+    elif str(v) == '3':
+        df_c[k] = np.log(X)
+    
+    elif str(v) == '3+':
+        X = STL(X, period=12).fit().trend
+        df_c[k] = np.log(X)
+
+    elif str(v) == '4':
+        df_c[k] = np.log(X) - np.log(X).shift(1)
+    
+    elif str(v) == '4+':
+        X = STL(X, period=12).fit().trend
+        df_c[k] = np.log(X) - np.log(X).shift(1)
+    else:
+        pass
+
+
+scaler = StandardScaler()
+Y = scaler.fit_transform(df_c)
+df_scaled = pd.DataFrame(Y, columns=df_c.columns, index=df_c.index)
+
+
+# GDP Growth
+gdp['GDP Growth'] = ( gdp['Real GDP']/gdp['Real GDP'].shift(4) - 1 ) * 100
+
+
+
+# Saving ######################################################################
+df_scaled.to_pickle('Data/monthly/data_st_2023_07.pkl')
+gdp.to_pickle('Data/quarterly/gdp_st_2023_07.pkl')
+
+# df_scaled.to_csv('Data/monthly/data_st_2023_07.csv', encoding='UTF-8')
+# gdp.to_csv('Data/quarterly/gdp_st_2023_07.csv', encoding='UTF-8')
